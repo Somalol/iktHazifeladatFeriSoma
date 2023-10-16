4f0a39e890d77ad8cf0f613e174bba9092a7d108
Tivadari Soma <tivsoma@gmail.com>
2023-10-16 09:24:33 +0200

    asd

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -2,5 +2,5 @@ import classes from "./Style.module.css"
 
 export default function Card(props) 
 {
-  return <div className={classes.Card}>{props.children}</div>;
+  return <div className={classes.card}>{props.children}</div>;
 }
